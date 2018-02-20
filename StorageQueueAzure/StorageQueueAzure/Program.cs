6758bd392340d1b066b04ec3173b0781fd21164b
Luiz Augusto Frambach <frambachluiz@gmail.com>
2018-02-20 19:06:51 -0300

    Commitando Todo processo

--- StorageQueueAzure/StorageQueueAzure/Program.cs
+++ StorageQueueAzure/StorageQueueAzure/Program.cs
@@ -1,7 +1,13 @@
 ﻿using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Queue;
 using System;
+using System.Collections.Generic;
 using System.Configuration;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace StorageQueueAzure
 {
@@ -18,21 +24,117 @@ namespace StorageQueueAzure
 
             cloudQueue.CreateIfNotExists();
 
-            for (int i = 1; i < 1000000; i++)
+            cloudQueue.FetchAttributes();
+
+            var option = new ParallelOptions
             {
-                var cloudQueueMessage = new CloudQueueMessage("Funcionando " + i);
-                cloudQueue.AddMessage(cloudQueueMessage);
+                MaxDegreeOfParallelism = 5
+            };
 
-                var retrieveMessage = cloudQueue.GetMessage();
+            #region solution 1
 
-                Console.WriteLine("Quantidade de linhas ja enviadas: " + retrieveMessage.AsString);
+            var allMessages = new List<Guid>();
 
-                cloudQueue.DeleteMessage(retrieveMessage);
+            while (true)
+            {
+                var teste = new TaskFactory();
+
+                var taskList = new List<Task>();
+
+                var batch = new List<CloudQueueMessage>();
+
+                var watch = Stopwatch.StartNew();
+
+                Parallel.For(1, 9, option, i =>
+                {
+                    taskList.Add(teste.StartNew(() =>
+                    {
+                        var messages = Task.Run(() => cloudQueue.GetMessagesAsync(32)).Result;
+
+                        batch.AddRange(messages);
+                    }));
+                });
+
+                Task.WaitAll(taskList.ToArray());
+
+                #endregion solution 1
+
+                Console.WriteLine(Environment.NewLine);
+
+                Console.WriteLine("Count: " + batch.Count);
+
+                Console.WriteLine("TempoDecorrido: " + watch.Elapsed);
+
+                foreach (var msg in batch)
+                {
+                    var currentId = Guid.Parse(msg.Id);
+
+                    if (allMessages.Any(x => x == currentId))
+                    {
+                        Console.WriteLine("DUPLICTY !!!");
+                        Console.ReadKey();
+                    }
+
+                    allMessages.Add(currentId);
+
+                    Console.WriteLine(msg.Id);
+                }
+
+                Thread.Sleep(100);
             }
 
-            // cloudQueue.DeleteMessage(retrieveMessage);
-            //5 min 500 iteraçoes
-            //30min  3000 iteraçoes
+            Console.Read();
         }
+
+        private static void InsertInQueue(CloudQueue queue)
+        {
+            var cloudQueueMessage = new CloudQueueMessage("{\"PaymentId\":\"492d1012-52b4-4e11-99bc-579636fcf57e\",\"ChangeType\":1} ");
+            queue.AddMessage(cloudQueueMessage);
+
+            Console.WriteLine("Adicionando: " + cloudQueueMessage);
+        }
+
+        private static void InsertParallel(CloudQueue queue)
+        {
+            var option = new ParallelOptions
+            {
+                MaxDegreeOfParallelism = 5
+            };
+
+            Parallel.For(1, 1500000, option, i =>
+            {
+                var cloudQueueMessage = new CloudQueueMessage("{\"PaymentId\":\"492d1012-52b4-4e11-99bc-579636fcf57e\",\"ChangeType\":1} numero: " + i);
+                queue.AddMessage(cloudQueueMessage);
+
+                Console.WriteLine("Adicionando: " + cloudQueueMessage);
+            });
+        }
+
+        private static CloudQueueMessage GetMessage(CloudQueue queue)
+        {
+            return queue.GetMessage(); //Message have 30 sec lifetime ,  and go back to queue
+        }
+
+        private static CloudQueueMessage GetAndDeleteMessage(CloudQueue queue)
+        {
+            var message = queue.GetMessage();
+
+            queue.DeleteMessage(message);
+
+            return message;
+        }
+
+        private static IEnumerable<CloudQueueMessage> GetMessages(CloudQueue queue)
+        {
+            var messages = queue.GetMessages(20);
+
+            foreach (var message in messages)
+            {
+                queue.DeleteMessage(message);
+            }
+
+            return messages;
+        }
+
     }
 }
