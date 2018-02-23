903e6dcbd9e90a45782cab931644dd8cf978de34
Luiz Augusto Frambach <lcruz@braspag.com.br>
2018-02-22 23:09:48 -0300

    ServicebusQueue

---
+++ ServiceBusQueue/ServiceBusQueue/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.ServiceBus.Messaging;
+using System;
+using System.Configuration;
+
+namespace ServiceBusQueue
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var serviceConnectionString =
+                ConfigurationManager.AppSettings["Microsoft.ServiceBus.ConnectionString"];
+
+            var queueClient = QueueClient.CreateFromConnectionString(serviceConnectionString); ;
+
+            var i = 1;
+
+            //Sending Messages
+            while (true)
+            {
+                var menssage = new BrokeredMessage();
+
+                menssage.ContentType = "mensagem " + i++;
+
+                queueClient.Send(menssage);
+
+                Console.WriteLine(menssage.ContentType + " enviada com sucesso!");
+            }
+
+            Console.Read();
+        }
+    }
+}
